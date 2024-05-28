122ccd28801823b02d5c72a6772aa04a2dd7799d
alexandre110282 <69106721+alexandre110282@users.noreply.github.com>
2024-05-28 14:29:28 +0200

    Correction

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <Router basename='/KasaP8'>
+    <Router >
         <App />
     </Router>
   </React.StrictMode>
